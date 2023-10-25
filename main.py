50b99000d6a30cb946306a2e198c79ac6e3722a1
malikovboo <malikovboo@gmail.com>
2023-10-26 00:07:15 +0300

    Final ver

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 detector = htm.handDetector(detectionCon=0.8)
-
+startX = None
 # Начальное положение центра ладони
 cx, cy = 0, 0
 # Флаг активации переключения слайдов
@@ -21,25 +21,36 @@ while True:
     lmList, _ = detector.findPosition(img)
 
     if len(lmList) != 0:
-        # Находим координаты центра ладони (можно использовать другую точку, если нужно)
-        x, y = lmList[9][1], lmList[9][2]
+        # Находим координаты x большого пальца
+        x = lmList[4][1]
 
         # Проверяем расстояние между указательным и средним пальцем
-        length_next, _, _ = detector.findDistance(4, 8, img, draw=False)
-        length_prev, _, _ = detector.findDistance(4, 12, img, draw=False)
-
+        length_1, _, _ = detector.findDistance(4, 8, img, draw=False)
+        length_2, _, _ = detector.findDistance(4, 12, img, draw=False)
 
         if slide_not_switched:
-            if length_next <= 70:
-                pyautogui.press('right')
-                slide_not_switched = False
-            elif length_prev <= 70:
-                pyautogui.press('left')
-                slide_not_switched = False
-        elif length_next > 70 and length_prev > 70:
+            if length_1 + length_2 <= 130:
+                # Если начальное положение не определено, устанавливаем его
+                if startX is None:
+                    startX = x
+
+                print("ready to slide")
+
+                if x > startX + 90:  # Если рука двигается вправо
+                    print("left slide")
+                    pyautogui.press('left')
+                    startX = None  # Обновляем начальное положение
+                    slide_not_switched = False
+
+                elif x < startX - 90:  # Если рука двигается влево
+                    print("right slide")
+                    pyautogui.press('right')
+                    startX = None  # Обновляем начальное положение
+                    slide_not_switched = False
+
+        elif length_1 + length_2 > 120:
             slide_not_switched = True
         print(slide_not_switched)
-
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
