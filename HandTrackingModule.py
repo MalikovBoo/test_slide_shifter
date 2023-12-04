b5230d1abaea06bb6243e85ec3c7505cc980195d
Denis <110720280+mor1nch@users.noreply.github.com>
2023-12-04 18:35:46 +0300

    documentation added

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,95 +1,137 @@
-
 import cv2
 import mediapipe as mp
 import time
 import math
 
 
-class handDetector():
-	def __init__(self, mode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, trackCon=0.5):
-		self.mode = mode
-		self.maxHands = maxHands
-		self.modelComplexity = modelComplexity
-		self.detectionCon = detectionCon
-		self.trackCon = trackCon
+class HandDetector:
+    """
+    Класс HandDetector использует библиотеку Mediapipe для обнаружения и отслеживания рук в видеопотоке.
+    """
 
-		self.mpHands = mp.solutions.hands
-		self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplexity, self.detectionCon, self.trackCon)
-		self.mpDraw = mp.solutions.drawing_utils
-		self.tipIds = [4, 8, 12, 16, 20]
+    def __init__(self, mode=False, max_hands=2, model_complexity=1, detection_con=0.5, track_con=0.5):
+        """
+        Инициализирует объект HandDetector с параметрами.
 
-	def findHands(self, img, draw=True):
-		imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-		self.results = self.hands.process(imgRGB)
-		#print(results.multi_hand_landmarks)
+        :param mode: Режим работы детектора рук (по умолчанию False).
+        :param max_hands: Максимальное количество рук для отслеживания (по умолчанию 2).
+        :param model_complexity: Сложность модели детектора рук (по умолчанию 1).
+        :param detection_con: Порог обнаружения руки (по умолчанию 0.5).
+        :param track_con: Порог отслеживания руки (по умолчанию 0.5).
+        """
+        self.lm_list = []  # Список для хранения координат ключевых точек руки
+        self.results = None
+        self.mode = mode
+        self.max_hands = max_hands
+        self.model_complexity = model_complexity
+        self.detection_con = detection_con
+        self.track_con = track_con
 
-		if self.results.multi_hand_landmarks:
-			for handLms in self.results.multi_hand_landmarks:
-				if draw:
-					self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
-		return img
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(self.mode, self.max_hands, self.model_complexity, self.detection_con,
+                                         self.track_con)
+        self.mp_draw = mp.solutions.drawing_utils
+        self.tip_ids = [4, 8, 12, 16, 20]
 
-	def findPosition(self, img, handNo=0, draw=True):
-		xList = []
-		yList = []
-		bbox = []
-		self.lmList = []
-		if self.results.multi_hand_landmarks:
-			myHand = self.results.multi_hand_landmarks[handNo]
-			for id, lm in enumerate(myHand.landmark):
-				#print(id, lm)
-				h, w, c = img.shape
-				cx, cy = int(lm.x*w), int(lm.y*h)
-				xList.append(cx)
-				yList.append(cy)
-				#print(id, cx, cy)
-				self.lmList.append([id, cx, cy])
-				if draw:
-					cv2.circle(img, (cx, cy), 5, (255,0,255), cv2.FILLED)
-			xmin, xmax = min(xList), max(xList)
-			ymin, ymax = min(yList), max(yList)
-			bbox = xmin, ymin, xmax, ymax
+    def find_hands(self, img, draw=True):
+        """
+        Находит и отображает руки на изображении.
 
-			if draw:
-				cv2.rectangle(img, (bbox[0]-20, bbox[1]-20), (bbox[2]+20, bbox[3]+20), (0, 255, 0), 2)
-		return self.lmList, bbox
+        :param img: Исходное изображение.
+        :param draw: Флаг отрисовки результатов (по умолчанию True).
+        :return: Изображение с отрисованными руками.
+        """
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(img_rgb)
 
-	def findDistance(self, p1, p2, img, draw=True):
-		x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
-		x2, y2 = self.lmList[p2][1], self.lmList[p2][2]
-		cx, cy = (x1+x2)//2, (y1+y2)//2
+        if self.results.multi_hand_landmarks:
+            for hand_lms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mp_draw.draw_landmarks(img, hand_lms, self.mp_hands.HAND_CONNECTIONS)
+        return img
 
-		if draw:
-			cv2.circle(img, (x1,y1), 15, (255,0,255), cv2.FILLED)
-			cv2.circle(img, (x2,y2), 15, (255,0,255), cv2.FILLED)
-			cv2.line(img, (x1,y1), (x2,y2), (255,0,255), 3)
-			cv2.circle(img, (cx,cy), 15, (255,0,255), cv2.FILLED)
+    def find_position(self, img, hand_no=0, draw=True):
+        """
+        Находит позицию ключевых точек (landmarks) и ограничивающий прямоугольник вокруг руки.
 
-		length = math.hypot(x2-x1, y2-y1)
-		return length, img, [x1, y1, x2, y2, cx, cy]
+        :param img: Исходное изображение.
+        :param hand_no: Индекс руки в случае, если обнаружено более одной (по умолчанию 0).
+        :param draw: Флаг отрисовки результатов (по умолчанию True).
+        :return: Список координат ключевых точек и координаты ограничивающего прямоугольника.
+        """
+        x_list = []
+        y_list = []
+        bbox = []
+        self.lm_list = []
+
+        if self.results.multi_hand_landmarks:
+            my_hand = self.results.multi_hand_landmarks[hand_no]
+            for id, lm in enumerate(my_hand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                x_list.append(cx)
+                y_list.append(cy)
+                self.lm_list.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+            xmin, xmax = min(x_list), max(x_list)
+            ymin, ymax = min(y_list), max(y_list)
+            bbox = xmin, ymin, xmax, ymax
+
+            if draw:
+                cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20), (bbox[2] + 20, bbox[3] + 20), (0, 255, 0), 2)
+        return self.lm_list, bbox
+
+    def find_distance(self, p1, p2, img, draw=True):
+        """
+        Находит расстояние между двумя точками на изображении и отрисовывает результат, если флаг draw установлен в True.
+
+        :param p1: Индекс первой точки в списке ключевых точек.
+        :param p2: Индекс второй точки в списке ключевых точек.
+        :param img: Исходное изображение.
+        :param draw: Флаг отрисовки результатов (по умолчанию True).
+        :return: Расстояние между точками, изображение с отрисованными элементами и координаты точек и их центра.
+        """
+        x1, y1 = self.lm_list[p1][1], self.lm_list[p1][2]
+        x2, y2 = self.lm_list[p2][1], self.lm_list[p2][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+        return length, img, [x1, y1, x2, y2, cx, cy]
 
 
 def main():
-	pTime = 0
-	cTime = 0
-	cap = cv2.VideoCapture(0)
-	detector = handDetector()
-	while True:
-		success, img = cap.read()
-		img = detector.findHands(img)
-		lmList = detector.findPosition(img)
-		if len(lmList) != 0:
-			print(lmList[1])
+    """
+    Основная функция программы для обработки видеопотока с веб-камеры и отображения руки с дополнительной информацией.
 
-		cTime = time.time()
-		fps = 1. / (cTime - pTime)
-		pTime = cTime
+    :return: None
+    """
+    p_time = 0
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector()
 
-		cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    while True:
+        success, img = cap.read()
+        img = detector.find_hands(img)
+        lm_list = detector.find_position(img)
+        if len(lm_list) != 0:
+            print(lm_list[1])
 
-		cv2.imshow("Image", img)
-		cv2.waitKey(1)
+        c_time = time.time()
+        fps = 1. / (c_time - p_time)
+        p_time = c_time
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+        cv2.imshow("Test Image", img)
+        cv2.waitKey(1)
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- main.py
+++ main.py
@@ -1,58 +1,66 @@
-
 import cv2
 import HandTrackingModule as htm
 import pyautogui
 
+# Инициализация видеокамеры
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
-detector = htm.handDetector(detectionCon=0.8)
-startX = None
+
+# Инициализация детектора руки
+detector = htm.HandDetector(detection_con=0.8)
+
+# Начальное положение большого пальца на ладони
+start_x = None
+
 # Начальное положение центра ладони
 cx, cy = 0, 0
+
 # Флаг активации переключения слайдов
 slide_switch_active = False
 slide_not_switched = True
 
 while True:
+    # Захват кадра с камеры
     success, img = cap.read()
     img = cv2.flip(img, 1)
-    img = detector.findHands(img)
-    lmList, _ = detector.findPosition(img)
 
-    if len(lmList) != 0:
+    # Обнаружение руки на кадре
+    img = detector.find_hands(img)
+    lm_list, _ = detector.find_position(img)
+
+    if len(lm_list) != 0:
         # Находим координаты x большого пальца
-        x = lmList[4][1]
+        x = lm_list[4][1]
 
         # Проверяем расстояние между указательным и средним пальцем
-        length_1, _, _ = detector.findDistance(4, 8, img, draw=False)
-        length_2, _, _ = detector.findDistance(4, 12, img, draw=False)
+        length_1, _, _ = detector.find_distance(4, 8, img, draw=False)
+        length_2, _, _ = detector.find_distance(4, 12, img, draw=False)
 
         if slide_not_switched:
             if length_1 + length_2 <= 130:
                 # Если начальное положение не определено, устанавливаем его
-                if startX is None:
-                    startX = x
+                if start_x is None:
+                    start_x = x
 
-                print("ready to slide")
+                # print("ready to slide")
 
-                if x > startX + 90:  # Если рука двигается вправо
-                    print("left slide")
+                if x > start_x + 90:  # Если рука двигается вправо
+                    print("Left slide")
                     pyautogui.press('left')
-                    startX = None  # Обновляем начальное положение
+                    start_x = None  # Обновляем начальное положение
                     slide_not_switched = False
 
-                elif x < startX - 90:  # Если рука двигается влево
-                    print("right slide")
+                elif x < start_x - 90:  # Если рука двигается влево
+                    print("Right slide")
                     pyautogui.press('right')
-                    startX = None  # Обновляем начальное положение
+                    start_x = None  # Обновляем начальное положение
                     slide_not_switched = False
 
         elif length_1 + length_2 > 120:
             slide_not_switched = True
-        print(slide_not_switched)
+        # print(slide_not_switched)
 
-    cv2.imshow("Image", img)
+    # Отображение окна с названием "Slide Shifter"
+    cv2.imshow("Slide Shifter", img)
     cv2.waitKey(1)
-
-
