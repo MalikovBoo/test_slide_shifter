f16c4d8f2c5b9eb2500b8779f153dfcf590de7f3
Denis <110720280+mor1nch@users.noreply.github.com>
2023-12-30 18:04:40 +0300

    code optimised

--- slide_shifter.py
+++ slide_shifter.py
@@ -3,9 +3,15 @@ import mediapipe
 import math
 import pyautogui
 import sys
-
-from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect)
+from enum import Enum
+from PySide6.QtCore import QMetaObject, QRect
 from PySide6.QtWidgets import (QLabel, QPushButton, QWidget, QMessageBox, QMainWindow, QApplication)
+
+
+class ButtonColors(Enum):
+    START = "rgb(107, 182, 84)"
+    STOP = "rgb(189, 54, 43)"
+    TURN_ON_OFF = "rgb(52, 120, 246)"
 
 
 class Ui_SlideShifter(QMainWindow):
@@ -17,128 +23,95 @@ class Ui_SlideShifter(QMainWindow):
         self.video_status = True
 
     def setupUi(self, SlideShifter):
-        """
-        Устанавливает пользовательский интерфейс для главного окна приложения.
-
-        :param SlideShifter: Экземпляр главного окна.
-        """
-
         if not SlideShifter.objectName():
             SlideShifter.setObjectName(u"SlideShifter")
         SlideShifter.resize(400, 720)
         SlideShifter.move(0, 0)
         SlideShifter.setStyleSheet(u"background-color: rgb(40, 40, 40);")
+
         self.centralwidget = QWidget(SlideShifter)
         self.centralwidget.setObjectName(u"centralwidget")
+
         self.start_button = QPushButton(self.centralwidget)
         self.start_button.setObjectName(u"start_button")
         self.start_button.setGeometry(QRect(30, 210, 340, 150))
-        self.start_button.setStyleSheet(u"background-color: rgb(107, 182, 84);\n"
-                                        "font: 300 24pt \"Helvetica Neue\";\n"
-                                        "color: rgb(0, 0, 0);")
+        self.set_button_style(self.start_button, ButtonColors.START)
+
         self.stop_button = QPushButton(self.centralwidget)
         self.stop_button.setObjectName(u"stop_button")
         self.stop_button.setGeometry(QRect(30, 370, 340, 150))
-        self.stop_button.setStyleSheet(u"background-color: rgb(189, 54, 43);\n"
-                                       "color: rgb(0, 0, 0);\n"
-                                       "font: 300 24pt \"Helvetica Neue\";")
+        self.set_button_style(self.stop_button, ButtonColors.STOP)
+
+        self.turn_on_off_button = QPushButton(self.centralwidget)
+        self.turn_on_off_button.setObjectName(u"turn_on_off_button")
+        self.turn_on_off_button.setGeometry(QRect(30, 530, 340, 70))
+        self.set_button_style(self.turn_on_off_button, ButtonColors.TURN_ON_OFF)
+
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
         self.label.setGeometry(QRect(80, 60, 240, 50))
         self.label.setStyleSheet(u"background-color: rgba(255, 255, 255, 0);\n"
                                  "color: rgb(255, 255, 255);\n"
-                                 "font: 900 36pt \"Futura\";")
-        self.turn_on_off_button = QPushButton(self.centralwidget)
-        self.turn_on_off_button.setObjectName(u"turn_on_off_button")
-        self.turn_on_off_button.setGeometry(QRect(30, 530, 340, 70))
-        self.turn_on_off_button.setStyleSheet(u"background-color: rgb(52, 120, 246);\n"
-                                              "color: rgb(0, 0, 0);\n"
-                                              "font: 300 24pt \"Helvetica Neue\";")
-        SlideShifter.setCentralWidget(self.centralwidget)
+                                 "font: 900 28pt \"Futura\";")
 
-        self.retranslateUi(SlideShifter)
+        SlideShifter.setCentralWidget(self.centralwidget)
+        self.setText(SlideShifter)
 
         QMetaObject.connectSlotsByName(SlideShifter)
-
         self.do_actions()
 
-    def retranslateUi(self, SlideShifter):
-        """
-        Переводит названия элементов интерфейса на заданный язык.
+    def set_button_style(self, button, color):
+        # Set the style for the buttons
+        button.setStyleSheet(f"background-color: {color.value};\n"
+                             "font: 300 24pt \"Helvetica Neue\";\n"
+                             "color: rgb(0, 0, 0);")
 
-        :param SlideShifter: Экземпляр главного окна.
-        """
-
-        SlideShifter.setWindowTitle(QCoreApplication.translate("SlideShifter", u"SlideShifterVideo", None))
-        self.start_button.setText(QCoreApplication.translate("SlideShifter", u"\u0421\u0442\u0430\u0440\u0442", None))
-        self.stop_button.setText(QCoreApplication.translate("SlideShifter", u"\u0421\u0442\u043e\u043f", None))
-        self.label.setText(QCoreApplication.translate("SlideShifter", u"Slide Shifter", None))
-        self.turn_on_off_button.setText(QCoreApplication.translate("SlideShifter",
-                                                                   u"\u0412\u043a\u043b\u044e\u0447\u0438\u0442\u044c/\u0412\u044b\u043a\u043b\u044e\u0447\u0438\u0442\u044c \u0432\u0438\u0434\u0435\u043e",
-                                                                   None))
+    def setText(self, SlideShifter):
+        # Set text for labels and buttons
+        SlideShifter.setWindowTitle("Slide Shifter")
+        self.start_button.setText("Start")
+        self.stop_button.setText("Stop")
+        self.label.setText("Slide Shifter")
+        self.turn_on_off_button.setText("Pause")
 
     def do_actions(self):
-        """
-        Назначает обработчики событий для кнопок в пользовательском интерфейсе.
-        """
-
+        # Connect button clicks to corresponding functions
         self.start_button.clicked.connect(self.start_hand_tracking)
         self.stop_button.clicked.connect(self.stop_hand_tracking)
-        self.turn_on_off_button.clicked.connect(self.turn_on_off_video)
+        self.turn_on_off_button.clicked.connect(self.pause_video)
 
     def start_hand_tracking(self):
-        """
-        Запускает отслеживание руки.
-        """
-
+        # Start hand tracking function
         self.is_started = True
         ht.is_started = self.is_started
         ht.hand_tracking_function()
 
     def stop_hand_tracking(self):
-        """
-        Останавливает отслеживание руки.
-        """
-
+        # Stop hand tracking function
         self.is_started = False
         ht.is_started = False
         ht.hand_tracking_function()
 
-    def turn_on_off_video(self):
-        """
-        Включает или выключает видеопоток.
-        """
-
+    def pause_video(self):
+        # Pause/unpause video
         self.cnt += 1
-        if self.cnt % 2 == 0:
-            self.video_status = True
-        else:
-            self.video_status = False
-
+        self.video_status = self.cnt % 2 == 0
         ht.video_on = self.video_status
         ht.hand_tracking_function()
 
     def closeEvent(self, event):
-        """
-        Обрабатывает событие закрытия главного окна.
-
-        :param event: Событие закрытия окна.
-        """
-
+        # Handle close event for the main window
         reply = self.show_quit_confirmation_dialog()
         if reply == QMessageBox.Yes:
             event.accept()
-            exit(0)
+            ht.cap.release()
+            cv2.destroyAllWindows()
+            sys.exit(0)
         else:
             event.ignore()
 
     def show_quit_confirmation_dialog(self):
-        """
-        Отображает диалоговое окно с подтверждением закрытия приложения.
-
-        :return: Ответ пользователя на вопрос о закрытии приложения.
-        """
-
+        # Show quit confirmation dialog
         exit_answer = QMessageBox.question(self, "Подтверждение закрытия",
                                            "Вы уверены, что хотите закрыть приложение?",
                                            QMessageBox.No | QMessageBox.Yes, QMessageBox.No)
@@ -146,21 +119,17 @@ class Ui_SlideShifter(QMainWindow):
 
 
 class HandDetector:
-    """
-    Класс HandDetector использует библиотеку Mediapipe для обнаружения и отслеживания рук в видеопотоке.
-    """
-
     def __init__(self, mode=False, max_hands=2, model_complexity=1, detection_con=0.5, track_con=0.5):
         """
-        Инициализирует объект HandDetector с параметрами.
+        Initializes an instance of the HandDetector class with specified parameters.
 
-        :param mode: Режим работы детектора рук (по умолчанию False).
-        :param max_hands: Максимальное количество рук для отслеживания (по умолчанию 2).
-        :param model_complexity: Сложность модели детектора рук (по умолчанию 1).
-        :param detection_con: Порог обнаружения руки (по умолчанию 0.5).
-        :param track_con: Порог отслеживания руки (по умолчанию 0.5).
+        :param mode: Hand detector mode (default is False).
+        :param max_hands: Maximum number of hands to track (default is 2).
+        :param model_complexity: Model complexity of the hand detector (default is 1).
+        :param detection_con: Detection confidence threshold for hand detection (default is 0.5).
+        :param track_con: Tracking confidence threshold for hand tracking (default is 0.5).
         """
-        self.lm_list = []  # Список для хранения координат ключевых точек руки
+        self.lm_list = []  # List to store coordinates of hand landmarks
         self.results = None
         self.mode = mode
         self.max_hands = max_hands
@@ -176,12 +145,14 @@ class HandDetector:
 
     def find_hands(self, image, draw=True):
         """
-        Находит и отображает руки на изображении.
+        Finds and visualizes hands on the image.
 
-        :param image: Исходное изображение.
-        :param draw: Флаг отрисовки результатов (по умолчанию True).
-        :return: Изображение с отрисованными руками.
+        :param image: Source image.
+        :param draw: Flag for drawing the results (default is True).
+        :return: Image with annotated hands.
         """
+        if image is None:
+            return None
         img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(img_rgb)
 
@@ -193,12 +164,12 @@ class HandDetector:
 
     def find_position(self, image, hand_no=0, draw=True):
         """
-        Находит позицию ключевых точек (landmarks) и ограничивающий прямоугольник вокруг руки.
+        Finds the position of hand landmarks and the bounding rectangle around the hand.
 
-        :param image: Исходное изображение.
-        :param hand_no: Индекс руки в случае, если обнаружено более одной (по умолчанию 0).
-        :param draw: Флаг отрисовки результатов (по умолчанию True).
-        :return: Список координат ключевых точек и координаты ограничивающего прямоугольника.
+        :param image: Source image.
+        :param hand_no: Index of the hand in case of multiple hands (default is 0).
+        :param draw: Flag for drawing the results (default is True).
+        :return: List of coordinates of hand landmarks and coordinates of the bounding rectangle.
         """
         x_list = []
         y_list = []
@@ -225,13 +196,12 @@ class HandDetector:
 
     def find_distance(self, index1, index2, image, draw=True):
         """
-        Находит расстояние между двумя точками на изображении и отрисовывает результат, если флаг draw установлен в True.
-
-        :param index1: Индекс первой точки в списке ключевых точек.
-        :param index2: Индекс второй точки в списке ключевых точек.
-        :param image: Исходное изображение.
-        :param draw: Флаг отрисовки результатов (по умолчанию True).
-        :return: Расстояние между точками, изображение с отрисованными элементами и координаты точек и их центра.
+        Finds the distance between two points on the image and visualizes the result if the draw flag is set to True.
+        :param index1: Index of the first point in the list of hand landmarks.
+        :param index2: Index of the second point in the list of hand landmarks.
+        :param image: Source image.
+        :param draw: Flag for drawing the results (default is True).
+        :return: Distance between the points, image with annotated elements, and coordinates of the points and their center.
         """
         x1, y1 = self.lm_list[index1][1], self.lm_list[index1][2]
         x2, y2 = self.lm_list[index2][1], self.lm_list[index2][2]
@@ -248,54 +218,70 @@ class HandDetector:
         return length, image, [x1, y1, x2, y2, cx, cy]
 
 
-class HandTracking():
+class HandTracking:
     def __init__(self):
-        self.video_window_title = "SlideShifterVideo"
+        """
+        Initializes an instance of the HandTracking class.
+
+        Attributes:
+            video_window_title (str): Title of the video window.
+            start_thumb_position (int or None): Initial position of the thumb.
+            slide_not_switched (bool): Flag indicating whether the slide is not switched.
+            video_on (bool): Flag indicating whether the video is on.
+            is_started (bool): Flag indicating whether hand tracking is started.
+            cap (cv2.VideoCapture): Video capture object.
+            detector (HandDetector): Instance of the HandDetector class for hand tracking.
+        """
+        self.video_window_title = "Slide Shifter Video"
         self.start_thumb_position = None
         self.slide_not_switched = True
         self.video_on = True
         self.is_started = False
+        self.cap = cv2.VideoCapture(0)
+        self.detector = HandDetector(detection_con=0.8)
 
     def hand_tracking_function(self):
-        cap = cv2.VideoCapture(0)
-        # Инициализация видеокамеры
+        """
+        Performs hand tracking using the initialized video capture and hand detector.
+
+        It initializes the video window, captures frames from the camera, detects hands, and controls the slide based on hand movements.
+        """
+        # Initialize the video window
         cv2.namedWindow(self.video_window_title, cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(self.video_window_title, 800, 600)
         cv2.moveWindow(self.video_window_title, 400, 0)
 
-        # Инициализация детектора руки
-        detector = HandDetector(detection_con=0.8)
-
         while self.video_on:
-            # Захват кадра с камеры
-            success, img = cap.read()
+            # Capture a frame from the camera
+            success, img = self.cap.read()
             image = cv2.flip(img, 1)
-            img = detector.find_hands(image)
+            img = self.detector.find_hands(image)
 
             if self.is_started:
-                # Обнаружение руки на кадре
-                lm_list, _ = detector.find_position(img)
+                # Detect hand landmarks in the frame
+                lm_list, _ = self.detector.find_position(img)
 
                 if len(lm_list) != 0:
-                    # Находим координаты x большого пальца
+                    # Find the x-coordinate of the thumb
                     x = lm_list[4][1]
 
-                    # Проверяем расстояние между указательным и средним пальцем
-                    length1, _, _ = detector.find_distance(4, 8, img, draw=False)
-                    length2, _, _ = detector.find_distance(4, 12, img, draw=False)
+                    # Check the distance between the index and middle fingers
+                    length1, _, _ = self.detector.find_distance(4, 8, img, draw=False)
+                    length2, _, _ = self.detector.find_distance(4, 12, img, draw=False)
 
                     if self.slide_not_switched:
                         if length1 + length2 <= 120:
-                            # Если начальное положение не определено, устанавливаем его
+                            # If the initial position is not defined, set it
                             if self.start_thumb_position is None:
                                 self.start_thumb_position = x
 
-                            if x > self.start_thumb_position + 90:  # Если рука двигается вправо
+                            if x > self.start_thumb_position + 90:  # If the hand moves to the right
                                 print("Left slide")
                                 pyautogui.press("left")
                                 self.start_thumb_position = None
                                 self.slide_not_switched = False
 
-                            elif x < self.start_thumb_position - 90:  # Если рука двигается влево
+                            elif x < self.start_thumb_position - 90:  # If the hand moves to the left
                                 print("Right slide")
                                 pyautogui.press("right")
                                 self.start_thumb_position = None
@@ -304,15 +290,10 @@ class HandTracking():
                     elif length1 + length2 > 120:
                         self.slide_not_switched = True
 
-            # Отображение окна
-            frame_re = cv2.resize(img, (1024, 576))
-            cv2.imshow(self.video_window_title, frame_re)
+            # Display the window
+            cv2.imshow(self.video_window_title, img)
 
             cv2.waitKey(1)
-
-        if not self.video_on:
-            cap.release()
-            cv2.destroyAllWindows()
 
 
 def run_application():
