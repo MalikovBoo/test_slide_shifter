0c1aba5bfc198212ca954106e2b425387d81e5f9
Denis <110720280+mor1nch@users.noreply.github.com>
2024-02-06 17:17:57 +0300

    design updated, camera changing added

--- slide_shifter.py
+++ slide_shifter.py
@@ -3,8 +3,11 @@ import mediapipe
 import math
 import pyautogui
 import sys
-from PySide6.QtCore import QMetaObject, Qt
-from PySide6.QtWidgets import QPushButton, QWidget, QMessageBox, QMainWindow, QApplication, QLabel, QVBoxLayout
+
+from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect)
+from PySide6.QtGui import QFont
+from PySide6.QtWidgets import (QApplication, QComboBox, QLabel, QMainWindow,
+                               QPushButton, QWidget, QMessageBox)
 
 
 class UiSlideShifter(QMainWindow):
@@ -14,64 +17,102 @@ class UiSlideShifter(QMainWindow):
     def setupUi(self, SlideShifter):
         if not SlideShifter.objectName():
             SlideShifter.setObjectName(u"SlideShifter")
-        SlideShifter.resize(400, 720)
         SlideShifter.move(0, 0)
-        SlideShifter.setStyleSheet(u"background-color: rgb(40, 40, 40);")
-
+        SlideShifter.resize(400, 720)
+        SlideShifter.setStyleSheet(u"background-color: rgb(40, 40, 40);\n"
+                                   "background-color: qradialgradient(spread:pad, cx:0.5, cy:0.497014, radius:0.485181, fx:0.496641, fy:0.443, stop:0 rgba(38, 90, 222, 255), stop:0.16 rgba(150, 161, 228, 255), stop:0.225 rgba(140, 163, 244, 255), stop:0.285 rgba(202, 189, 247, 255), stop:0.345 rgba(153, 172, 255, 255), stop:0.415 rgba(208, 187, 231, 255), stop:0.52 rgba(123, 118, 228, 255), stop:0.57 rgba(98, 128, 208, 255), stop:0.635 rgba(121, 139, 200, 255), stop:0.695 rgba(137, 146, 201, 255), stop:0.75 rgba(129, 151, 216, 255), stop:0.815 rgba(154, 164, 208, 255), stop:0.88 rgba(137, 148, 205, 255), stop:0.935 rgba(216, 217, 224, 255), stop:1 rgba(255, 255, 255, 255));")
         self.centralwidget = QWidget(SlideShifter)
         self.centralwidget.setObjectName(u"centralwidget")
-
-        # Create a vertical layout to arrange widgets from top to bottom
-        vertical_layout = QVBoxLayout(self.centralwidget)
-        vertical_layout.setObjectName(u"vertical_layout")
-
-        # Label
+        self.start_button = QPushButton(self.centralwidget)
+        self.start_button.setObjectName(u"start_button")
+        self.start_button.setGeometry(QRect(50, 250, 300, 150))
+        self.start_button.setStyleSheet(u"background-color: rgb(128, 185, 116);\n"
+                                        "font: 400 24pt \"Helvetica Neue\";\n"
+                                        "color: rgb(0, 0, 0);")
+        self.stop_button = QPushButton(self.centralwidget)
+        self.stop_button.setObjectName(u"stop_button")
+        self.stop_button.setGeometry(QRect(50, 420, 300, 150))
+        self.stop_button.setStyleSheet(u"background-color: rgb(234, 77, 62);\n"
+                                       "color: rgb(0, 0, 0);\n"
+                                       "font: 400 24pt \"Helvetica Neue\";")
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(102, 100, 195, 39))
+        font = QFont()
+        font.setFamilies([u"Futura"])
+        font.setPointSize(30)
+        font.setWeight(QFont.Black)
+        font.setItalic(False)
+        self.label.setFont(font)
         self.label.setStyleSheet(u"background-color: rgba(255, 255, 255, 0);\n"
-                                 "color: rgb(255, 255, 255);\n"
-                                 "font: 900 28pt \"Futura\";")
-        self.label.setAlignment(Qt.AlignCenter)
-        vertical_layout.addWidget(self.label)
-
-        # "Start" button
-        self.start_button = QPushButton(self.centralwidget)
-        self.start_button.setStyleSheet(f"background-color: rgb(107, 182, 84);\n"
-                                        "font: 300 24pt \"Helvetica Neue\";\n"
-                                        "color: rgb(0, 0, 0);")
-        self.start_button.setText("Start")
-        self.start_button.setMinimumHeight(120)
-        vertical_layout.addWidget(self.start_button)
-
-        # "Stop" button
-        self.stop_button = QPushButton(self.centralwidget)
-        self.stop_button.setStyleSheet(f"background-color: rgb(189, 54, 43);\n"
-                                       "font: 300 24pt \"Helvetica Neue\";\n"
-                                       "color: rgb(0, 0, 0);")
-        self.stop_button.setText("Stop")
-        self.stop_button.setMinimumHeight(120)
-        vertical_layout.addWidget(self.stop_button)
-
-        # Adjust the bottom margin to shift the buttons slightly higher
-        vertical_layout.setContentsMargins(20, 0, 20, 100)
-
+                                 "color: rgb(24, 24, 24);\n"
+                                 "font: 900 30pt \"Futura\";")
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(132, 680, 136, 20))
+        font1 = QFont()
+        font1.setFamilies([u"Futura"])
+        font1.setPointSize(16)
+        font1.setWeight(QFont.DemiBold)
+        font1.setItalic(False)
+        self.label_2.setFont(font1)
+        self.label_2.setStyleSheet(u"background-color: rgba(255, 255, 255, 0);\n"
+                                   "color: rgb(84, 85, 85);\n"
+                                   "font: 600 16pt \"Futura\";\n"
+                                   "")
+        self.comboBox = QComboBox(self.centralwidget)
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setGeometry(QRect(310, 20, 80, 21))
+        self.comboBox.setStyleSheet(u"color: rgb(0, 0, 0);\n"
+                                    "background-color: rgb(151, 152, 152);\n"
+                                    "font: 300 14pt \"Helvetica Neue\";")
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(240, 20, 61, 19))
+        self.label_3.setStyleSheet(u"background-color: rgba(255, 255, 255, 0);\n"
+                                   "color: rgb(32, 32, 32);\n"
+                                   "font: 400 16pt \"Helvetica Neue\";\n"
+                                   "")
         SlideShifter.setCentralWidget(self.centralwidget)
-        self.setText(SlideShifter)
+        self.get_connected_cameras()
+        self.comboBox.activated.connect(self.activated)
+        self.retranslateUi(SlideShifter)
 
         QMetaObject.connectSlotsByName(SlideShifter)
-        self.do_actions()
+        self.connect_buttons()
 
-    def setText(self, SlideShifter):
-        # Set text for labels and buttons
-        SlideShifter.setWindowTitle("Slide Shifter")
-        self.start_button.setText("Start")
-        self.stop_button.setText("Stop")
-        self.label.setText("Slide Shifter")
+    def retranslateUi(self, SlideShifter):
+        SlideShifter.setWindowTitle(QCoreApplication.translate("SlideShifter", u"Slide Shifter", None))
+        self.start_button.setText(
+            QCoreApplication.translate("SlideShifter", u"\u041d\u0430\u0447\u0430\u0442\u044c", None))
+        self.stop_button.setText(QCoreApplication.translate("SlideShifter",
+                                                            u"\u041f\u0440\u0438\u043e\u0441\u0442\u0430\u043d\u043e\u0432\u0438\u0442\u044c ",
+                                                            None))
+        self.label.setText(QCoreApplication.translate("SlideShifter", u"Slide Shifter", None))
+        self.label_2.setText(QCoreApplication.translate("SlideShifter", u"by Singularity Hub", None))
 
-    def do_actions(self):
+        self.label_3.setText(QCoreApplication.translate("SlideShifter", u"Camera:", None))
+
+    def connect_buttons(self):
         # Connect button clicks to corresponding functions
         self.start_button.clicked.connect(self.start_hand_tracking)
         self.stop_button.clicked.connect(self.stop_hand_tracking)
+
+    def get_connected_cameras(self):
+        cameras = []
+        for i in range(10):
+            cap = cv2.VideoCapture(i)
+            if not cap.isOpened():
+                break
+            cameras.append(i)
+            cap.release()
+            self.comboBox.addItem("")
+            self.comboBox.setItemText(i, QCoreApplication.translate("SlideShifter", f"№{i + 1}", None))
+        ht.cameras = cameras
+
+    def activated(self, index):
+        ht.camera_index = index
+        ht.hand_tracking_function()
 
     def start_hand_tracking(self):
         # Start hand tracking function
@@ -88,8 +129,8 @@ class UiSlideShifter(QMainWindow):
         reply = self.show_quit_confirmation_dialog()
         if reply == QMessageBox.Yes:
             event.accept()
-            ht.cap.release()
-            cv2.destroyAllWindows()
+            ht.video_on = False
+            ht.hand_tracking_function()
             sys.exit(0)
         else:
             event.ignore()
@@ -221,7 +262,8 @@ class HandTracking:
         self.slide_not_switched = True
         self.video_on = True
         self.is_started = False
-        self.cap = cv2.VideoCapture(0)
+        self.cameras = [0, ]
+        self.camera_index = 0
         self.detector = HandDetector(detection_con=0.8)
 
     def hand_tracking_function(self):
@@ -231,13 +273,19 @@ class HandTracking:
         It initializes the video window, captures frames from the camera, detects hands, and controls the slide based on hand movements.
         """
         # Initialize the video window
+        cap = cv2.VideoCapture(self.cameras[self.camera_index])
+
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) // 2
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) // 2
+        __, _ = cap.read()
+        cv2.imshow(self.video_window_title, _)
         cv2.namedWindow(self.video_window_title, cv2.WINDOW_NORMAL)
-        cv2.resizeWindow(self.video_window_title, 800, 600)
+        cv2.resizeWindow(self.video_window_title, width, height)
         cv2.moveWindow(self.video_window_title, 400, 0)
 
         while self.video_on:
             # Capture a frame from the camera
-            success, img = self.cap.read()
+            success, img = cap.read()
             image = cv2.flip(img, 1)
             img = self.detector.find_hands(image)
 
@@ -278,6 +326,9 @@ class HandTracking:
             cv2.imshow(self.video_window_title, img)
 
             cv2.waitKey(1)
+        else:
+            cap.release()
+            cv2.destroyAllWindows()
 
 
 def run_application():
